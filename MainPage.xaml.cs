82983b480b7002577c6ad1ac5438e9b06513abfc
F3T1W <particular0010abyss@gmail.com>
2023-08-13 14:30:31 +0500

    Добавьте файлы проекта.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace EldenTracker
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
