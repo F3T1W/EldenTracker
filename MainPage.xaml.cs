c71b56978deecb796dc6cc60395c9fbec9972d07
F3T1W <particular0010abyss@gmail.com>
2023-08-14 21:52:38 +0500

    Made default scrolling position per launch

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
 
 namespace EldenTracker
 {
@@ -12,7 +16,14 @@ namespace EldenTracker
         public MainPage()
         {
             InitializeComponent();
+
+            mapImage.SizeChanged += MapImage_SizeChanged;
+
             CreateComponents();
+
+            Uri combinedImageUri = new Uri("ms-appx:///Resources/Map/Images/FullMap.png");
+            BitmapImage bitmapImage = new BitmapImage(combinedImageUri);
+            mapImage.Source = bitmapImage;
 
             mapImage.PointerPressed += Map.MapImage_PointerPressed;
             mapImage.PointerMoved += Map.MapImage_PointerMoved;
@@ -22,6 +33,19 @@ namespace EldenTracker
         private void CreateComponents()
         {
             Map = new Map(scrollViewer, mapImage);
+        }
+
+        private void MapImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // This event is raised when the image has been measured and arranged
+            double initialHorizontalOffset = (mapImage.ActualWidth - scrollViewer.ActualWidth) / 3;
+            double initialVerticalOffset = (mapImage.ActualHeight - scrollViewer.ActualHeight) / 2;
+
+            // Set the initial scroll position
+            scrollViewer.ChangeView(initialHorizontalOffset, initialVerticalOffset, null);
+
+            // Unsubscribe from the event, as we only need to do this once
+            mapImage.SizeChanged -= MapImage_SizeChanged;
         }
     }
 }
